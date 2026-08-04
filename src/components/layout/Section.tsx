36272896ba80ad3a88690cb0e6e5c448bb6037b4
the-young-swag <xplizit.nm7@gmail.com>
2026-08-04 10:42:01 +0800

    feat: add Section component

---
+++ src/components/layout/Section.tsx
@@ -0,0 +1,22 @@
+import type { PropsWithChildren } from "react";
+import clsx from "clsx";
+
+type SectionProps = PropsWithChildren<{
+    id?: string;
+    className?: string;
+}>;
+
+export function Section({
+    children,
+    id,
+    className,
+}: SectionProps){
+    return (
+        <section
+            id={id}
+            className={clsx("py-16 md:py-24", className)}
+            >
+                {children}
+        </section>
+    )
+}
