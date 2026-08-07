d6aa861a52099fc8ccfc51ff5ee23d2811bc027f
the-young-swag <xplizit.nm7@gmail.com>
2026-08-07 19:58:40 +0800

    feat(github): add year selector component

--- src/components/github/YearSelector.tsx
+++ src/components/github/YearSelector.tsx
@@ -1,3 +1,18 @@
 export function YearSelector() {
-    return <div>Year Selector</div>;
+    return (
+        <div className = "flex gap-2">
+            
+            <button>2026</button>
+
+            <button>2025</button>
+
+            <button>2024</button>
+
+            <button>2023</button>
+
+            <button>2022</button>
+
+        </div>
+    
+    );
 }
